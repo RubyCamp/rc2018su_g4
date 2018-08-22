0eed9a2b6044ee11a1c48466bba82c6b99965ede
0Kate <o.keito317@gmail.com>
2018-08-22 14:23:07 +0900

    update controllermain.rb

--- controllermain.rb
+++ controllermain.rb
@@ -9,11 +9,8 @@ require_relative 'bullet'
 
 
 c1 = Controller1.new(0, 300, Image.new(20, 20, C_RED))
-
 c2 = Controller2.new(580, 300, Image.new(20, 20, C_BLUE))
-
 c3 = Controller3.new(300, 580, Image.new(20, 20, C_GREEN))
-
 c4 = Controller4.new(300, 0, Image.new(20, 20, C_YELLOW))
 
 Window.width = 600
